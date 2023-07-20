13ff4b8268bc511a7e0c1618b01397eddf6fc354
sange tsering <sangekelsang098@gmail.com>
2023-07-20 12:47:12 +0530

    program updated

--- Chapter1/Agecalculator.c
+++ Chapter1/Agecalculator.c
@@ -3,22 +3,79 @@
 #include<stdio.h> 
 int main()
 {
-    int py,pb,xm,xy,xd,mp,td,bd;
+    int curdate,curmonth,curyear;
+	int birthdate,birthmonth,birthyear;
     
     printf("enter the details\n********************************\n");
-    printf("enter the present year \n");
-    scanf("%d",&py);
-    printf("enter ur year of birth \n");
-    scanf("%d",&pb);
-    printf("enter number of  month passed this year \n");
-    scanf("%d",&mp);
-    printf("enter todays date only \n");
-    scanf("%d",&td);
-    printf("enter ur date of bith  \n");
-    scanf("%d",&bd);
-    xy=py-pb;
-    xm=12-mp;
-    xd=td-bd;
+    printf("enter present date in format YY MM DD :\n");
+    scanf("%d %d %d",&curyear,&curmonth,&curdate);
     
-    printf("ur %d years %d months %d days old.",xy,xm,xd);
+    printf("\nenter your date of birth in format YY MM DD :\n");
+    scanf("%d %d %d",&birthyear,&birthmonth,&birthdate);
+    
+    
+    //calculating based on certain conditions of birthday 
+    if(birthmonth>curmonth)
+    {
+    	birthyear = (curyear - birthyear)-1;
+    	birthmonth = curmonth;
+    	if(birthdate == curdate)
+    	      birthdate = 0;
+    	else
+    	{
+    		if(birthdate>curdate)
+    		{
+    			birthdate = curdate;
+			}
+			
+			else
+			  birthdate = curdate - birthdate;
+			
+		}
+	}
+	else
+	{
+		if(birthmonth<curmonth)
+		{
+			birthyear = curyear - birthyear;
+			birthmonth = curmonth - birthmonth;
+			
+		if(birthdate == curdate)
+    	      birthdate = 0;
+    	else
+    	{
+    		if(birthdate>curdate)
+    		{
+    			birthdate = curdate;
+			}
+			
+			else
+			  birthdate = curdate - birthdate;
+			
+		}
+	  }
+	  else
+	  {
+	  	birthyear = curyear - birthyear;
+			birthmonth = 0;
+			
+		if(birthdate == curdate)
+    	      birthdate = 0;
+    	else
+    	{
+    		if(birthdate>curdate)
+    		{
+    			birthdate = curdate;
+			}
+			
+			else
+			  birthdate = curdate - birthdate;
+			
+		}
+	  }
+	}
+    
+    printf("\n Your %d years %d months %d days old.",birthyear,birthmonth,birthdate);
+    
+    return 0;
 }
