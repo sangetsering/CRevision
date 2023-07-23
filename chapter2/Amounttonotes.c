d8963cea5f6112307c76d1476944de79be16427e
sange tsering <sangekelsang098@gmail.com>
2023-07-23 09:32:38 +0530

    code updated

--- chapter2/Amounttonotes.c
+++ chapter2/Amounttonotes.c
@@ -10,42 +10,46 @@ int main()
     scanf("%d", &amount);
 
     printf("%d is equal to:\n", amount);
-
+    
+    while(amount>0)
+    {
+    	
     if (amount >= 100)
     {
         times = amount / 100;
         amount = amount % 100;
-        printf("%d 100 notes\n", times);
+        printf("%d of 100Rs notes\n", times);
     }
     else if (amount < 100 && amount >= 50)
     {
         times = amount / 50;
         amount = amount % 50;
-        printf("%d 50 notes\n", times);
+        printf("%d of 50Rs notes\n", times);
     }
     else if (amount < 50 && amount >= 10)
     {
         times = amount / 10;
         amount = amount % 10;
-        printf("%d 10 notes\n", times);
+        printf("%d of 10Rs notes\n", times);
     }
     else if (amount < 10 && amount >= 5)
     {
         times = amount / 5;
         amount = amount % 5;
-        printf("%d 5 notes\n", times);
+        printf("%d of 5Rs notes\n", times);
     }
     else if (amount < 5 && amount >= 2)
     {
         times = amount / 2;
         amount = amount % 2;
-        printf("%d 2 notes\n", times);
+        printf("%d of 2Rs notes\n", times);
     }
     else
     {
         times = amount;
-        printf("%d 1 notes\n", times);
+        amount = amount%1;
+        printf("%d of 1Rs notes\n", times);
     }
-
+  }
     return 0;
 }
