14c6f979670d5b7a5365ee131ea413eeef003c76
sange tsering <sangekelsang098@gmail.com>
2023-08-07 19:53:02 +0530

    Code update in Seriesevaluate.c program

--- chapter7/Seriesevaluate.c
+++ chapter7/Seriesevaluate.c
@@ -1,66 +1,94 @@
 //program to evaluate the function to 0.0001% accuracy
+//sinx = x-x^3/3! + X^5/5! -X^7/7!........
+//cosx = 1- x^2/2! + x^4/4! - x^6/6!+........
+//sum = 1 + (1/2)^2 + (1/3)^3 + (1/4)^4 + ......
 
 #include<stdio.h>
+#include<math.h>
+#define accuracy 0.000001
 
-//exponential function
-float exponent(float,int);
+
+//Declaration of functions
+
+int factorial(int);       //to find factorial
+double sin_series(double); //to calculate the sinx value
+double cos_series(double);  //to calculate the cosx value
+float sums(void);           // to calulate the sum of series
+
+//main function begins
 int main()
 {
-   float x,sinx,cosx=1,sum=1,accuracy=0;
-   int i=0;
+   double x,sinx,cosx;
+   float sum;
    
    printf("Enter the value of X(angle): ");
    scanf("%f",&x);
    
-   sinx = x;
-   //sinx computation
-   while(sinx != accuracy)
-   {
-     	sinx = sinx + sinx*(-exponent(x,(2*i+1))/fact(2*i+1));
-     	i++;
-     	accuracy = sinx - sinx*(0.000001);
-   }
-   
-   accuracy =0;
-   i =1;
-   //cosx computation
-   while(cosx != accuracy)
-   {
-     	cosx = cosx + cosx*(-exponent(x,(2*i))/fact(2*i));
-     	i++;
-     	accuracy = cosx - cosx*(0.000001);
-   }
-   	
-   //sum computation
-   accuracy = 0;
-   i = 1;
-   while(sum != accuracy)
-   {
-     	sum = sum + sum*exponent(1.0/++i,i);
-     	accuracy = sum - sum*(0.000001);
-   }
-   
-   printf("\nsinx : %f  cosx : %f  sum : %f",sinx,cosx,sum);
-   
-   return 0;
+   //converting x into radian
+   x = x*(3.14/180); 
+    
+   //function calling 
+    sinx = sin_series(x);
+    
+    cosx = cos_series(x);
+    
+	printf("\nActuaL sinx : %lf  Approx sinx : %lf",sin(x),sinx);
+    printf("\nActuaL cosx : %lf  Approx cosx : %lf",cos(x),cosx);
+    printf("\n 1+(1/2)^2+(1/3)^3+......  Approx sum : %f",sums());
+    
+    return 0;
 }
 
-int fact(int n)
+int factorial(int n)
 {
 	if(n == 1)
 	  return 1;
 	else
-	  return n*fact(n-1);
+	  return n*factorial(n-1);
 }
 
-float exponent(float x, int n)
+double sin_series(double x)
 {
-	int i;float num = 1.0;
+    double sinx = x,y=x;
+    int i=1;
 	
-	for(i=0;i<n;i++)
+	while(fabs(sinx)>accuracy)
 	{
-	   	 num = num * x ;
-	}
+	    y*= (-1*x*x)/factorial(2*i+1);
+		sinx+= y;
+		i++;
+	}	
+    
+    return sinx;
+}
+
+double cos_series(double y)
+{
+    double cosx = 1.00 ,x = 1.00;
+    int i=1;
 	
-	return num;
+	while(fabs(cosx)>accuracy)
+	{
+		x*= (-1*y*y)/factorial(2*i);	
+	    cosx = cosx + x;
+		i++;
+	}	
+    
+    return cosx;
+	
+}
+
+float sums(void)
+{
+    float sum = 1.00;
+    int i=2;
+	
+	while(fabs(sum)>accuracy)
+	{
+	    sum = sum + pow(1/i,i);
+		i++;
+	}	
+    
+    return sum;
+	
 }
